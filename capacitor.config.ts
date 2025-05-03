4069e3ae5b84dbdd94480f155b30f4f7a7d25310
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-05-02 23:03:37 -0400

    New App creation, pages added, standalone configuration has been applied, translate plugins added, json translate files added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: '06_burguer_queen',
+  appId: 'io.ionic.burguer',
+  appName: 'Brguer Queen',
   webDir: 'www'
 };
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Device } from '@capacitor/device';
+import { Platform } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,31 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+
+  public load: boolean;
+  
+  constructor(
+    private platform: Platform,
+     private translate: TranslateService
+  ) {
+
+    this.translate.setDefaultLang('es');
+    this.load = false;
+    this.initApp();
+
+  }
+
+  initApp(){
+    this.platform.ready().then( async () => {
+      const language = await Device.getLanguageCode();
+      //console.log(language.value);
+      
+      if (language.value) {
+        this.translate.use(language.value.slice(0, 2));
+      } else {
+        
+      }
+      this.load = true;
+    })
+  }
 }
